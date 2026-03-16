c874952bd6adc99a065a4abbcb7fc447ea1debd1
manpreet singh <manpreeysinghms1080@gmail.com>
2026-03-16 18:01:37 +0530

    U

--- app/src/main/java/org/example/model/UserinfoDto.java
+++ app/src/main/java/org/example/model/UserinfoDto.java
@@ -5,16 +5,22 @@ import org.example.entities.userinfo;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
+import io.micrometer.common.lang.NonNull;
+
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UserinfoDto extends userinfo {
     
 
+    @NonNull
     private String userName;
     
+    @NonNull
     private String lastName;
 
+    @NonNull
     private Long phoneNumber;
 
+    @NonNull
     private String email;
 }
